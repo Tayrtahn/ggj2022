5a56f6f312247f6cf43c3870e985f3ee5043665c
Chris Gengler <tayrtahn@gmail.com>
2022-01-27 14:26:04 -0500

    Expanded functionality of PlayerManager

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -5,9 +5,52 @@ using UnityEngine.InputSystem;
 
 public class PlayerManager : MonoBehaviour
 {
+    private PaddleController[] paddleControllers;
+
+    [SerializeField]
+    private int _maxPlayers = 2;
+
+    [SerializeField]
+    [Tooltip("Minimum number of players needed to start game")]
+    private int _minPlayers = 2;
+
+    private void OnValidate()
+    {
+        if (paddleControllers == null)
+        {
+            paddleControllers = new PaddleController[_maxPlayers];
+        }
+        else
+        {
+            System.Array.Resize(ref paddleControllers, _maxPlayers);
+        }
+    }
+
     public void OnPlayerJoined(PlayerInput playerInput)
     {
         PaddleController controller = playerInput.GetComponent<PaddleController>();
+
+        paddleControllers[playerInput.playerIndex] = controller;
+
         controller.Setup(playerInput);
+    }
+
+    public int JoinedPlayerCount
+    {
+        get
+        {
+            int count = 0;
+            for (int i = 0; i < paddleControllers.Length; ++i)
+            {
+                if (paddleControllers[i] != null)
+                    ++count;
+            }
+            return count;
+        }
+    }
+
+    public bool HasEnoughPlayers()
+    {
+        return JoinedPlayerCount >= _minPlayers;
     }
 }
