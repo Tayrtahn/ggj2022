00d72d68f317f8729039be8d3a7ea1fd2f1064c7
Jonathan Niemi <jontniemi@gmail.com>
2022-01-28 18:33:52 -0500

    Pickup edits, & spinning arena
    -created separate arrow materials for the speed up and slow down pickups
    -added constant rotation to Arena

--- Assets/Scripts/Arena.cs
+++ Assets/Scripts/Arena.cs
@@ -6,6 +6,8 @@ public class Arena : MonoBehaviour
 {
     [SerializeField]
     private float _radius = 20;
+    [SerializeField]
+    private float _spinSpeed = 1.0f;
 
     public float Radius => _radius;
 
@@ -14,5 +16,10 @@ public class Arena : MonoBehaviour
     public float DistanceFromCenter(Vector3 position)
     {
         return Vector3.Distance(CenterPosition, position);
+    }
+
+    public void Update()
+    {
+        transform.Rotate(Vector3.forward * _spinSpeed * Time.deltaTime, Space.World);
     }
 }
