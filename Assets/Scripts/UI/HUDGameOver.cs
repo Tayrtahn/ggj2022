1dab036b0f4eb74f4add500c20dcc0897f5cb81d
Jonathan Niemi <jontniemi@gmail.com>
2022-01-29 16:38:30 -0500

    Quick n dirty Game Over
    -appears and goes back to title screen when a player reaches 10 points

--- Assets/Scripts/UI/HUDGameOver.cs
+++ Assets/Scripts/UI/HUDGameOver.cs
@@ -2,14 +2,38 @@ using UnityEngine;
 
 public class HUDGameOver : MonoBehaviour
 {
+    [SerializeField] GameObject activateGo;
+    [SerializeField] float loadDelay = 3.0f;
+    float timer;
+
+    private void OnEnable()
+    {
+        activateGo.SetActive(true);
+        TimeManager.AddTimescaleSource(this, 0.0f);
+    }
+
+    private void OnDisable()
+    {
+        activateGo.SetActive(false);
+    }
+
     void Start()
     {
+        timer = loadDelay;
         SFXManager.PlaySound(SoundType.GameOver);
-        Invoke("GotoTitle", 2f);
+    }
+
+    private void Update()
+    {
+        if (timer <= 0)
+            GotoTitle();
+        else
+            timer -= Time.unscaledDeltaTime;
     }
 
     void GotoTitle()
     {
+        TimeManager.RemoveTimescaleSource(this);
         SceneManager.Goto(Constants.TITLE_SCENE);
     }
 }
