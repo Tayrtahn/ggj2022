553e7236147e424f478f523626ba433423f9f812
Jonathan Niemi <jontniemi@gmail.com>
2022-01-29 15:52:15 -0500

    Score font, HUDGame score update support
    -added another font type for scores
    -HUDGame can be hooked up to the GameManager.OnGoal now, and will accumulate scores

--- Assets/Scripts/UI/HUDGame.cs
+++ Assets/Scripts/UI/HUDGame.cs
@@ -1,9 +1,20 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HUDGame : MonoBehaviour
 {
-    void Start()
+    [SerializeField] Text[] scoreTexts;
+    int[] scores;
+
+    void Awake()
     {
-        
+        scores = new int[4];
+    }
+
+    public void AccumulateScore(int playerId)
+    {
+        iTween.PunchScale(scoreTexts[playerId].gameObject, Vector3.one * 1.00001f, 1f);
+        scores[playerId] += 1;
+        scoreTexts[playerId].text = scores[playerId].ToString();
     }
 }
